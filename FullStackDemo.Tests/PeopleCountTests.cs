8f85278c34b48cb281855b3385696bc11393c40c
gregsommerville <me@gregsommerville.com>
2019-03-30 13:37:41 -0400

    Added unit tests

---
+++ FullStackDemo.Tests/PeopleCountTests.cs
@@ -0,0 +1,18 @@
+﻿using FullStackDemo.Models.RequestHandlers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
+
+namespace FullStackDemo.Tests
+{
+    [TestClass]
+    public class PeopleCountTests
+    {
+        [TestMethod]
+        public async Task NonZeroCount()
+        {
+            // retrieve a known person
+            var count = await PeopleCountRequests.GetNumberOfPeopleAsync();
+            Assert.AreNotEqual(0, count);
+        }
+    }
+}
---
+++ FullStackDemo.Tests/PeopleGetTests.cs
@@ -0,0 +1,30 @@
+﻿using FullStackDemo.Models.DAL;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
+
+namespace FullStackDemo.Tests
+{
+    [TestClass]
+    public class PeopleGetTests
+    {
+        [TestMethod]
+        public async Task DontFindPerson()
+        {
+            using (var context = new PeopleContext())
+            {
+                var people = await context.GetPeopleMatchingAsync("zzzzzzz");
+                Assert.IsTrue(people.Count == 0);
+            }            
+        }
+
+        [TestMethod]
+        public async Task FindKnownPerson()
+        {
+            using (var context = new PeopleContext())
+            {
+                var people = await context.GetPeopleMatchingAsync("washington");
+                Assert.IsTrue(people.Count == 1);
+            }
+        }
+    }
+}
