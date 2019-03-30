128f1b2151e91877b714f6aacd6c3f5f92269b26
gregsommerville <me@gregsommerville.com>
2019-03-30 13:44:23 -0400

    Added a couple of unit tests

---
+++ FullStackDemo.Tests/PersonAddTests.cs
@@ -0,0 +1,53 @@
+﻿using FullStackDemo.Models.DAL;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FullStackDemo.Tests
+{
+    [TestClass]
+    public class PersonAddTests
+    {
+        [TestMethod]
+        public void AddInvalid()
+        {
+            var newPerson = new Person()
+            {
+                FirstName = "Timothy"
+            };
+
+            using (var context = new PeopleContext())
+            {
+                try
+                {
+                    context.AddPerson(newPerson);
+                    // should throw an exception
+                    Assert.Fail();
+                }
+                catch
+                {
+                }
+            }
+        }
+
+        [TestMethod]
+        public void AddValid()
+        {
+            var newPerson = new Person()
+            {
+                FirstName = "John",
+                LastName = "Kennedy"
+            };
+
+            using (var context = new PeopleContext())
+            {
+                try
+                {
+                    context.AddPerson(newPerson);
+                }
+                catch
+                {
+                    Assert.Fail();
+                }
+            }
+        }
+    }
+}
