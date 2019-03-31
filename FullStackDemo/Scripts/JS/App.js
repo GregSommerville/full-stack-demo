1168e73e9045b3c22edca233a1e8520e6cffc441
gregsommerville <me@gregsommerville.com>
2019-03-31 13:39:32 -0400

    Initial code for React components

--- FullStackDemo/Scripts/JS/App.js
+++ FullStackDemo/Scripts/JS/App.js
@@ -11,8 +11,61 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var React = require("react");
     var ReactDOM = require("react-dom");
-    var App = function () { return (React.createElement("div", null,
-        React.createElement("h1", null, "Hi There!"))); };
     ReactDOM.render(React.createElement(App, null), document.getElementById("reactContainer"));
+    function App() {
+        var _a = React.useState(true), isShowingSearch = _a[0], setShowingSearch = _a[1];
+        function onStateButtonClick(newState) {
+            setShowingSearch(newState);
+        }
+        if (isShowingSearch) {
+            return (React.createElement(React.Fragment, null,
+                React.createElement(ModeControl, { showingSearch: isShowingSearch, onClick: onStateButtonClick }),
+                React.createElement(SearchControl, null)));
+        }
+        else {
+            return (React.createElement(React.Fragment, null,
+                React.createElement(ModeControl, { showingSearch: isShowingSearch, onClick: onStateButtonClick }),
+                React.createElement(NewPerson, null)));
+        }
+    }
+    function ModeControl(props) {
+        var searchClasses = "btn btn-primary active";
+        var addClasses = "btn btn-secondary";
+        if (!props.showingSearch) {
+            searchClasses = "btn btn-secondary";
+            addClasses = "btn btn-primary active";
+        }
+        return (React.createElement("div", { className: 'appModeSwitcher' },
+            React.createElement("div", { className: "btn-group", role: "group", "aria-label": "Mode buttons" },
+                React.createElement("button", { type: "button", className: searchClasses, onClick: function () { return props.onClick(true); } }, "Search"),
+                React.createElement("button", { type: "button", className: addClasses, onClick: function () { return props.onClick(false); } }, "Add Person"))));
+    }
+    function SearchControl(props) {
+        return (React.createElement("div", { className: 'form-group' },
+            "Find:",
+            React.createElement("input", { type: "text", className: "form-control", id: "txtSearch", placeholder: 'type letters to search for' })));
+    }
+    function NewPerson(props) {
+        return (React.createElement("form", null,
+            React.createElement("div", { className: 'form-group' },
+                React.createElement("label", { htmlFor: 'FirstName' }, "First Name:"),
+                React.createElement("input", { type: "text", className: "form-control", id: "FirstName" })),
+            React.createElement("div", { className: 'form-group' },
+                React.createElement("label", { htmlFor: 'LastName' }, "Last Name:"),
+                React.createElement("input", { type: "text", className: "form-control", id: "LastName" })),
+            React.createElement("div", { className: 'form-group' },
+                React.createElement("label", { htmlFor: 'Address' }, "Address:"),
+                React.createElement("input", { type: "text", className: "form-control", id: "Address" })),
+            React.createElement("div", { className: 'form-group' },
+                React.createElement("label", { htmlFor: 'Age' }, "Age:"),
+                React.createElement("input", { type: "text", className: "form-control", id: "Age" })),
+            React.createElement("div", { className: 'form-group' },
+                React.createElement("label", { htmlFor: 'Interests' }, "Interests:"),
+                React.createElement("input", { type: "text", className: "form-control", id: "Interests" })),
+            React.createElement("div", { className: 'form-group' },
+                React.createElement("label", { htmlFor: 'PortraitURL' }, "Portrait URL:"),
+                React.createElement("input", { type: "text", className: "form-control", id: "PortraitURL" })),
+            React.createElement("button", { type: "submit", className: 'btn btn-primary' }, "Submit")));
+    }
 });
 //# sourceMappingURL=App.js.map
--- FullStackDemo/Scripts/Typescript/App.tsx
+++ FullStackDemo/Scripts/Typescript/App.tsx
@@ -1,10 +1,89 @@
 ﻿import * as React from "react";
 import * as ReactDOM from "react-dom";
 
-const App = () => (
-    <div>
-        <h1>Hi There!</h1>
-    </div>
-);
-
 ReactDOM.render(<App />, document.getElementById("reactContainer"));
+
+
+function App() {
+    const [isShowingSearch, setShowingSearch] = React.useState(true);
+
+    function onStateButtonClick(newState: boolean): void {
+        setShowingSearch(newState);
+    }
+
+    if (isShowingSearch) {
+        return (
+            <>
+                <ModeControl showingSearch={isShowingSearch} onClick={onStateButtonClick} />
+                <SearchControl />
+            </>
+        );
+    }
+    else {
+        return (
+            <>
+                <ModeControl showingSearch={isShowingSearch} onClick={onStateButtonClick} />
+                <NewPerson />
+            </>
+        );
+    }
+}
+
+function ModeControl(props) {
+    let searchClasses: string = "btn btn-primary active";
+    let addClasses: string = "btn btn-secondary";
+    if (!props.showingSearch) {
+        searchClasses = "btn btn-secondary";
+        addClasses = "btn btn-primary active";
+    }
+
+    return (
+        <div className='appModeSwitcher'>
+            <div className="btn-group" role="group" aria-label="Mode buttons">
+                <button type="button" className={searchClasses} onClick={() => props.onClick(true)} >Search</button>
+                <button type="button" className={addClasses} onClick={() => props.onClick(false)}>Add Person</button>
+            </div>
+        </div>
+    );
+}
+
+function SearchControl(props) {
+    return (
+        <div className='form-group'>
+            Find: 
+            <input type="text" className="form-control" id="txtSearch" placeholder='type letters to search for' />
+        </div>
+        );
+}
+
+function NewPerson(props) {
+    return (
+        <form>
+            <div className='form-group'>
+                <label htmlFor='FirstName'>First Name:</label>
+                <input type="text" className="form-control" id="FirstName" />
+            </div>
+            <div className='form-group'>
+                <label htmlFor='LastName'>Last Name:</label>
+                <input type="text" className="form-control" id="LastName" />
+            </div>
+            <div className='form-group'>
+                <label htmlFor='Address'>Address:</label>
+                <input type="text" className="form-control" id="Address" />
+            </div>
+            <div className='form-group'>
+                <label htmlFor='Age'>Age:</label>
+                <input type="text" className="form-control" id="Age" />
+            </div>
+            <div className='form-group'>
+                <label htmlFor='Interests'>Interests:</label>
+                <input type="text" className="form-control" id="Interests" />
+            </div>
+            <div className='form-group'>
+                <label htmlFor='PortraitURL'>Portrait URL:</label>
+                <input type="text" className="form-control" id="PortraitURL" />
+            </div>
+            <button type="submit" className='btn btn-primary'>Submit</button>
+        </form>
+    );
+}
